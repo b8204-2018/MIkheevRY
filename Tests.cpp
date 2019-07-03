77099683ff6a02b0e8c045e6ceb6b3010453b580
Roman <opter12345@gmail.com>
2019-07-03 11:14:41 +1000

    create more tests

--- Tests.cpp
+++ Tests.cpp
@@ -33,6 +33,31 @@ TEST(Parser, parser_back_mismatched_brackets) {
     ExpressionParser n;
     ASSERT_THROW(n.parse(s), logic_error);
 }
+TEST(Parser,left_priority){
+    string s = "( 2*2 + 5 ) /  7";
+    ExpressionParser n;
+    list<string> result = n.parse(s);
+    string result2 = "";
+    for (string x : result) { result2 += x; }
+    EXPECT_EQ(result2, "22*5+7/");
+}
+TEST(Parser,True_priority){
+    string s = "( 4/2*2 + 5 ) /  7";
+    ExpressionParser n;
+    list<string> result = n.parse(s);
+    string result2 = "";
+    for (string x : result) { result2 += x; }
+    EXPECT_EQ(result2, "42/2*5+7/");
+
+}
+TEST(Parser,right_priority){
+    string s = "( 2 + 5/2 ) /  7";
+    ExpressionParser n;
+    list<string> result = n.parse(s);
+    string result2 = "";
+    for (string x : result) { result2 += x; }
+    EXPECT_EQ(result2, "252/+7/");
+}
 
 TEST(Calc, calc_is_working) {
     string s = "( 5 + 2 ) /  7";
